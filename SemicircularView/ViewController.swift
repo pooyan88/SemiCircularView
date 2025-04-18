63b8013ba2f7412bc50caefdd0f252bd0d55a8da
Pooyan J <pooyanjavidi@gmail.com>
2025-04-18 18:51:48 +0330

    Initial Commit

---
+++ SemicircularView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SemicircularView
+//
+//  Created by Pooyan J on 4/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
