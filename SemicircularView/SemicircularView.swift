9d34720c4e48cb3179694e2a3835a73678923147
Pooyan J <pooyanjavidi@gmail.com>
2025-04-19 13:21:25 +0330

    Fixed percentage.

--- SemicircularView/SemicircularView.swift
+++ SemicircularView/SemicircularView.swift
@@ -18,19 +18,26 @@ class SemiCircularProgressView: UIView {
             setNeedsDisplay() // Redraw whenever segments are updated
         }
     }
-    
+
     override func draw(_ rect: CGRect) {
         guard !segments.isEmpty else { return }
 
-        let center = CGPoint(x: rect.midX, y: rect.maxY) // Bottom center
-        let radius = rect.width / 2
-        let startAngle = CGFloat.pi // 180 degrees
-        var currentAngle = startAngle
-        
-        for segment in segments {
-            let endAngle = currentAngle + (CGFloat.pi * (segment.percentage / 100))
-            let path = UIBezierPath()
+        // Clear any existing layers to avoid stacking
+        layer.sublayers?.forEach { $0.removeFromSuperlayer() }
 
+        let center = CGPoint(x: rect.midX, y: rect.maxY)
+        let radius = rect.width / 2
+        let startAngle = CGFloat.pi
+        var currentAngle = startAngle
+
+        // Normalize based on total percentage
+        let total = segments.reduce(0) { $0 + $1.percentage }
+
+        for segment in segments {
+            let angle = CGFloat.pi * (segment.percentage / total)
+            let endAngle = currentAngle + angle
+
+            let path = UIBezierPath()
             path.move(to: center)
             path.addArc(withCenter: center, radius: radius, startAngle: currentAngle, endAngle: endAngle, clockwise: true)
             path.close()
--- SemicircularView/ViewController.swift
+++ SemicircularView/ViewController.swift
@@ -11,17 +11,14 @@ class ViewController: UIViewController {
     
     let semiCircularView = SemiCircularProgressView(frame: CGRect(x: 50, y: 200, width: 300, height: 150))
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         semiCircularView.segments = [
             SemiCircularSegment(color: .red, percentage: 20),
             SemiCircularSegment(color: .orange, percentage: 15),
-            SemiCircularSegment(color: .yellow, percentage: 10),
-            SemiCircularSegment(color: .green, percentage: 10),
-            SemiCircularSegment(color: .blue, percentage: 15),
-            SemiCircularSegment(color: .purple, percentage: 10)
+            SemiCircularSegment(color: .yellow, percentage: 20),
+            SemiCircularSegment(color: .green, percentage: 20),
         ]
 
         view.addSubview(semiCircularView)
